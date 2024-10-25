43ece2919b0f604fee71f3c33f1809cd0c144d97
MinkNhat <nquyenminhnhat0503@gmail.com>
2024-10-25 14:38:00 +0700

    update

---
+++ SaleApp/index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import dao
+
+app = Flask(__name__)
+
+
+# Định tuyến domain
+@app.route("/")
+def index():
+    categories = dao.load_categories()
+    return render_template("index.html", categories=categories)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
